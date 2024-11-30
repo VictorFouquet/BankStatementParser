f3f4c558092e750e6a8ac104f77ccf449eb3db41
victor <victor.fouquet@gmail.com>
2024-11-30 18:13:38 +0100

    Write result to xlsx

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import re
 from PIL import Image
 from pdf2image import convert_from_path
 from pytesseract import pytesseract
+import xlsxwriter
 
 
 def extract_text(_img):
     doc = convert_from_path(_img)
     path, fileName = os.path.split(_img)
-    
+    workbook = xlsxwriter.Workbook('Expenses01.xlsx')
+    worksheet = workbook.add_worksheet()
+
     fileBaseName, fileExtension = os.path.splitext(fileName)
     pattern = r"\b\d{2}/\d{2}.*\d*,\d{2}\b"
     expense = r"\b\d{2}/\d{2} (ACHAT|VIREMENT.*À|PRELEVEMENT).*\d*,\d{2}\b"
 
+    expenses = 0
+    incomes = 0
+    row = 0
     for page_number, page_data in enumerate(doc):
         txt = pytesseract.image_to_string(page_data, lang='fra').encode('utf-8')
         decoded = txt.decode('utf-8')
@@ -23,8 +29,21 @@ def extract_text(_img):
                 amount = line.split(" ")[-1]
                 if re.fullmatch(expense, line):
                     print(date + ": -" + amount)
+                    expenses += float(amount.replace(',', '.'))
+                    worksheet.write(row, 0, date)
+                    worksheet.write(row, 1, float(amount.replace(',', '.')))
+                    worksheet.write(row, 2, 'expense')
                 else:
                     print(date + ": +" + amount)
+                    incomes += float(amount.replace(',', '.'))
+                    worksheet.write(row, 0, date)
+                    worksheet.write(row, 1, float(amount.replace(',', '.')))
+                    worksheet.write(row, 2, 'income')
+                row += 1
+    workbook.close()
+    print("Incomes: " + str(incomes))
+    print("Expenses: " + str(expenses))
+    print("Balance: " + str(incomes - expenses))
 
 
 if __name__ == '__main__':
